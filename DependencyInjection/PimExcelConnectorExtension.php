1f9b01b666b3c295ada0556b4171e6a25d919ffd
Antoine Guigan <antoine@akeneo.com>
2014-02-03 15:01:26 +0100

    Created bundle

---
+++ DependencyInjection/PimExcelConnectorExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Pim\Bundle\ExcelConnectorBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class PimExcelConnectorExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+    }
+}
---
+++ PimExcelConnectorBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Pim\Bundle\ExcelConnectorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PimExcelConnectorBundle extends Bundle
+{
+}
