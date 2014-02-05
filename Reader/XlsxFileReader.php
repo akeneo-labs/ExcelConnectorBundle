344863f7d7c37cfa3d33ff500c1bf36b5dec8afc
Antoine Guigan <antoine@akeneo.com>
2014-02-05 10:32:49 +0100

    Fixed PHPDoc

--- Reader/XlsxFileReader.php
+++ Reader/XlsxFileReader.php
@@ -3,7 +3,8 @@
 namespace Pim\Bundle\ExcelConnectorBundle\Reader;
 
 /**
- *
+ * XLSX file reader
+ * 
  * @author    Antoine Guigan <antoine@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
