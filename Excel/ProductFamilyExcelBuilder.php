d04897a206884e9bc1c3281be18f083b10d71054
Antoine Guigan <antoine@akeneo.com>
2014-02-07 17:12:39 +0100

    Added specs

--- Excel/ProductFamilyExcelBuilder.php
+++ Excel/ProductFamilyExcelBuilder.php
@@ -3,13 +3,13 @@
 namespace Pim\Bundle\ExcelConnectorBundle\Excel;
 
 /**
- * Excel builder for products
+ * Excel builder for products, with one tab per family
  *
  * @author    Antoine Guigan <antoine@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class ProductExcelBuilder extends AbstractExcelBuilder
+class ProductFamilyExcelBuilder extends AbstractExcelBuilder
 {
     /**
      * {@inheritdoc}
---
+++ spec/Pim/Bundle/ExcelConnectorBundle/Excel/ProductFamilyExcelBuilderSpec.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace spec\Pim\Bundle\ExcelConnectorBundle\Excel;
+
+class ProductFamilyExcelBuilderSpec extends ExcelBuilderBehavior
+{
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType('Pim\Bundle\ExcelConnectorBundle\Excel\ProductFamilyExcelBuilder');
+    }
+
+    public function it_creates_one_tab_per_family()
+    {
+        $this->add(['family' => 'FAMILY1', 'col1' => 'value1', 'col2' => 'value2']);
+        $this->add(['family' => 'FAMILY1', 'col1' => 'value3', 'col2' => 'value4']);
+        $this->add(['family' => 'FAMILY2', 'col1' => 'value5', 'col3' => 'value6']);
+        $this->add(['family' => 'FAMILY2', 'col1' => 'value7', 'col3' => 'value8']);
+        $excelObject = $this->getExcelObject();
+        $excelObject->shouldHaveCellRow('FAMILY1', 1, ['family', 'col1', 'col2']);
+        $excelObject->shouldHaveCellRow('FAMILY1', 2, ['FAMILY1', 'value1', 'value2']);
+        $excelObject->shouldHaveCellRow('FAMILY1', 3, ['FAMILY1', 'value3', 'value4']);
+        $excelObject->shouldHaveCellRow('FAMILY2', 1, ['family', 'col1', 'col3']);
+        $excelObject->shouldHaveCellRow('FAMILY2', 2, ['FAMILY2', 'value5', 'value6']);
+        $excelObject->shouldHaveCellRow('FAMILY2', 3, ['FAMILY2', 'value7', 'value8']);
+    }
+}
