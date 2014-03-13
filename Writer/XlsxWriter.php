eaf9953f8402b1385caca9e8b7eea6fa4f1e843d
Antoine Guigan <antoine@akeneo.com>
2014-03-13 15:48:11 +0100

    Upgraded for v1.1.x of the PIM

--- Writer/XlsxWriter.php
+++ Writer/XlsxWriter.php
@@ -14,7 +14,7 @@ use Pim\Bundle\ExcelConnectorBundle\Excel\ExcelBuilderFactory;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class XlsxWriter extends FileWriter implements InitializableInterface
+class XlsxWriter extends FileWriter
 {
     /**
      * @Assert\NotBlank(groups={"Execution"})
