5320af2d84bb0e3fb1f958c462ed213e29f3d65d
Matthieu Viel <matthieu.viel@akeneo.com>
2014-05-09 11:06:24 +0200

    RowBuilder implementation

--- Excel/Reader/RowBuilder.php
+++ Excel/Reader/RowBuilder.php
@@ -9,9 +9,15 @@ namespace Pim\Bundle\ExcelConnectorBundle\Excel\Reader;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-
 class RowBuilder
 {
+
+    /**
+     *
+     * @var array
+     */
+    protected $values = array();
+
     /**
      * Adds a value to the row
      *
@@ -20,7 +26,13 @@ class RowBuilder
      */
     public function addValue($columnIndex, $value)
     {
-        throw new \Exception('NOT IMPLEMENTED');
+
+        for ($i = 0; $i <= $columnIndex; $i++) {
+            if (!array_key_exists($i, $this->values)) {
+                $this->values[$i] = '';
+            }
+        }
+        $this->values[$columnIndex] = $value;
     }
 
     /**
@@ -30,6 +42,18 @@ class RowBuilder
      */
     public function getData()
     {
-        throw new \Exception('NOT IMPLEMENTED');
+        $arrayTemp = array_reverse($this->values, true);
+
+        foreach ($arrayTemp as $key => $value) {
+
+            if ('' === $value) {
+                unset($arrayTemp[$key]);
+            } else {
+                break;
+            }
+        }
+
+        return array_reverse($arrayTemp);
     }
+
 }
